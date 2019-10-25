1d4343784ecafd3c4f2f0aad362b53f3193fd8b0
Gabriel <morrisgaby987@googlemail.com>
2019-10-25 12:03:15 +0300

    'adding-to-do'

--- src/DescModal.js
+++ src/DescModal.js
@@ -3,16 +3,30 @@ import React from "react";
 class DescModal extends React.Component {
   render() {
     var project = this.props.project;
+	var {values} = this.props.project;
+		const valueItems = values.map( (value) =><span className='value'> 
+								{value.slice(0,10)} {value.length > 10 ? '...': null}
+							</span> )
     return (
-        <div style={{
+        <div clasName='projectModal' style={{
             width: '100%',
             height:' 100%',
             display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
+            justifyContent: 'space-around',
+            alignItems: 'center',
+			flexDirection: 'column'
 
-        }}>{project.story}
-        </div>
+        }}>
+			<div> 
+			<h3>{project.title}</h3> 
+			</div>
+			<div>
+				{valueItems}
+			</div>
+			<div>
+			{project.story}
+			</div>
+		</div>
     );
   }
 }
